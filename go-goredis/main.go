49cfde7dd36b14db7cc8570687af62f59bf02d74
bob <bob.liu@happyelements.com>
2021-08-08 13:42:30 +0800

    change redis ip

--- go-goredis/main.go
+++ go-goredis/main.go
@@ -12,7 +12,7 @@ func main() {
 	var ctx = context.Background()
 	rdb := redis.NewClient(&redis.Options{
 		// redis 服务的地址和端口号
-		Addr:     "139.155.77.236:6678",
+		Addr:     "127.0.0.1:6678",
 		Password: "",
 		DB:       0,
 	})
@@ -20,8 +20,8 @@ func main() {
 	/* 设置username的值为bob
 	expiration 过期时间， 0代表不设置过期时间
 	过期时间到达后，key删除
-	 */
-   // 保存数据
+	*/
+	// 保存数据
 	err := rdb.Set(ctx, "username", "bob", 0).Err()
 	if err != nil {
 		// 错误信息输出，控制台或终端
@@ -42,14 +42,13 @@ func main() {
 	if err == redis.Nil {
 		// 用户没有被禁用
 		fmt.Println("key2 does not exists")
-	}else if err != nil {
+	} else if err != nil {
 		// redis 出现异常
 		panic(err)
-	}else {
+	} else {
 		// 获取到用户禁用数据
-		fmt.Println("key2",val2)
+		fmt.Println("key2", val2)
 	}
-
 
 	defer rdb.Close()
 
